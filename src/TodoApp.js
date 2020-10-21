17390a8e40e34ca3adb3922b9879258f168e4a3e
arnab <arnab10panda@gmail.com>
2020-10-21 21:55:18 +0530

    Adding TodoForm and Connecting with hooks

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -5,6 +5,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Grid from '@material-ui/core/Grid';
 import TodoList from './TodoList';
+import TodoForm from './TodoForm';
 function TodoApp(){
     const initialTodos = [
         {id:1, task:"Clean FishTank", completed:false},
@@ -12,6 +13,9 @@ function TodoApp(){
         {id:3, task:"Grow Beared", completed:false}
     ]
     const [todos,setTodos] = useState(initialTodos);
+    const addTodo = (newTodoText) => {
+        setTodos([...todos, { id:4, task:newTodoText, completed:false } ]);
+    }
     return(
         <Paper
             style={{
@@ -27,6 +31,7 @@ function TodoApp(){
                     <Typography color='inherit'>TODOS WITH HOOKS</Typography>
                 </Toolbar>
             </AppBar>
+            <TodoForm addTodo={addTodo} />
             <TodoList todos={todos} />
         </Paper>
     );
