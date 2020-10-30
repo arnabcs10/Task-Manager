0cfbbb1cca6b949ee449b44b8bc9a13192034529
arnab <arnab10panda@gmail.com>
2020-10-30 20:55:21 +0530

    Splitting the contexts into dispatch and todos

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -2,10 +2,10 @@ import React,{useContext} from 'react'
 import Paper from '@material-ui/core/Paper';
 import  TextField  from '@material-ui/core/TextField';
 import useInputState from './hooks/useInputState';
-import {TodosContext} from './contexts/todos.context';
+import {DispatchContext} from './contexts/todos.context';
 
  function TodoForm() {
-     const {dispatch} = useContext(TodosContext);
+     const dispatch = useContext(DispatchContext);
     const [value,handleChange,reset] = useInputState("");
     return (
         <Paper style={{margin:"1rem 0",padding:"0 1rem"}}>
--- src/contexts/todos.context.js
+++ src/contexts/todos.context.js
@@ -7,13 +7,16 @@ const defaultTodos = [
 ];
 
 export const TodosContext = createContext(defaultTodos);
+export const DispatchContext = createContext();
 
 export function TodosProvider(props){
     const [todos,dispatch] = useReducer(todoReducer,defaultTodos);
 
     return(
-        <TodosContext.Provider value={{todos,dispatch}}>
-            {props.children}
+        <TodosContext.Provider value={todos}>
+            <DispatchContext.Provider value={dispatch}>
+                {props.children}
+            </DispatchContext.Provider>
         </TodosContext.Provider>
     );
 }
