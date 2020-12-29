3e91fddd30e2f6e57f2925ac95359df878d6351b
arnab <arnab10panda@gmail.com>
2020-12-29 14:51:16 +0530

    EditTodoForm made multiline and button added

--- src/EditTodoForm.js
+++ src/EditTodoForm.js
@@ -1,27 +1,34 @@
 import React,{useContext} from 'react';
 import useInputState from './hooks/useInputState';
 import  TextField  from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 import {DispatchContext} from './contexts/todos.context';
 
 function EditTodoForm(props) {
     const dispatch = useContext(DispatchContext);
     const [value,handleChange] = useInputState(props.task);
     return (
-        <form onSubmit={(e)=>{
-            e.preventDefault();
-            dispatch({ type: "EDIT", newTask: value, id: props.id })
-            props.toggleIsEditing();
-        }} 
-         style={{marginLeft:"1rem",width:"50%"}}
+        <form  
+         style={{width:"100%", display: "flex", flexWrap: "wrap", justifyContent: "flex-end"}}
         >
             <TextField 
                 value={value} 
                 onChange={handleChange} 
+                multiline={true}
                 margin='normal'
                 label='Edit'
                 fullWidth
                 autoFocus
             />
+            <Button variant="contained" color="primary"
+                onClick={(e)=>{
+                e.preventDefault();
+                dispatch({ type: "EDIT", newTask: value, id: props.id })
+                props.toggleIsEditing();
+            }}
+            >
+                Done
+            </Button>
         </form>
     );
 }
